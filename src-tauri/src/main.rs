48f30bebe5165c42ef944c24f15500d8aa13d86b
Parker McMullin <parker.mcmullin01@gmail.com>
2021-06-19 17:41:29 -0700

    Register remaining commands.

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -8,7 +8,12 @@ mod models;
 
 fn main() {
   tauri::Builder::default()
-    .invoke_handler(tauri::generate_handler![cmd::get_all_todos])
+    .invoke_handler(tauri::generate_handler![
+      cmd::get_all_todos,
+      cmd::create_todo,
+      cmd::update_todo,
+      cmd::remove_todo
+    ])
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
 }
