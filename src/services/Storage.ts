cf0f94b688cc2bca6e144b2c5b8ef2c8a4f3ed82
Parker McMullin <parker.mcmullin01@gmail.com>
2021-06-22 08:49:17 -0700

    Attempt to use Tauri API in module (SvelteKit error).

--- src/services/Storage.ts
+++ src/services/Storage.ts
@@ -1,22 +1,25 @@
-import { invoke } from '@tauri-apps/api/tauri';
+// TODO: this doesn't work either
+// following https://github.com/tauri-apps/tauri/pull/1517
+import { tauri } from '@tauri-apps/api';
+// import { invoke } from '@tauri-apps/api/tauri';
 import type { Todo } from '../models/Todo';
 
 async function all(): Promise<Todo[]> {
-  const all: string = await invoke('get_all_todos');
+  const all: string = await tauri.invoke('get_all_todos');
   return JSON.parse(all);
 }
 
 async function create(title: string): Promise<Todo> {
-  const todo: string = await invoke('create_todo', { title });
+  const todo: string = await tauri.invoke('create_todo', { title });
   return JSON.parse(todo);
 }
 
 function update(todo: Todo): Promise<Todo> {
-  return invoke('update_todo', { todo });
+  return tauri.invoke('update_todo', { todo });
 }
 
 function remove(id: string): Promise<boolean> {
-  return invoke('remove_odo', { id });
+  return tauri.invoke('remove_odo', { id });
 }
 
 export default {
