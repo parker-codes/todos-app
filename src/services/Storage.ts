85c41d23b785d624799a7337ef0657d41af13b89
Parker McMullin <parker.mcmullin01@gmail.com>
2021-07-24 13:44:10 -0700

    Fix imports.

--- src/services/Storage.ts
+++ src/services/Storage.ts
@@ -1,25 +1,22 @@
-// TODO: this doesn't work either
-// following https://github.com/tauri-apps/tauri/pull/1517
-import { tauri } from '@tauri-apps/api';
-// import { invoke } from '@tauri-apps/api/tauri';
+import { invoke } from '@tauri-apps/api/tauri';
 import type { Todo } from '../models/Todo';
 
 async function all(): Promise<Todo[]> {
-  const all: string = await tauri.invoke('get_all_todos');
+  const all: string = await invoke('get_all_todos');
   return JSON.parse(all);
 }
 
 async function create(title: string): Promise<Todo> {
-  const todo: string = await tauri.invoke('create_todo', { title });
+  const todo: string = await invoke('create_todo', { title });
   return JSON.parse(todo);
 }
 
 function update(todo: Todo): Promise<Todo> {
-  return tauri.invoke('update_todo', { todo });
+  return invoke('update_todo', { todo });
 }
 
 function remove(id: string): Promise<boolean> {
-  return tauri.invoke('remove_odo', { id });
+  return invoke('remove_odo', { id });
 }
 
 export default {
